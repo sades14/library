8f6060d2fbca73df17e97df1cda949fb46bda4ab
sades14 <125224254+sades14@users.noreply.github.com>
2023-02-17 20:44:41 -0500

    Update source.lua

--- source.lua
+++ source.lua
@@ -1096,7 +1096,7 @@ end
 
 local themes = {
     Default = {
-        ["Accent"] = Color3.fromRGB(113, 93, 133),
+        ["Accent"] = Color3.fromRGB(84,140,183),
         ["Window Background"] = Color3.fromRGB(30, 30, 30),
         ["Window Border"] = Color3.fromRGB(45, 45, 45),
         ["Tab Background"] = Color3.fromRGB(20, 20, 20),
@@ -1109,22 +1109,6 @@ local themes = {
         ["Object Background"] = Color3.fromRGB(25, 25, 25),
         ["Object Border"] = Color3.fromRGB(35, 35, 35),
         ["Dropdown Option Background"] = Color3.fromRGB(19, 19, 19)
-    },
-
-    Midnight = {
-        ["Accent"] = Color3.fromRGB(100, 59, 154),
-        ["Window Background"] = Color3.fromRGB(30, 30, 36),
-        ["Window Border"] = Color3.fromRGB(45, 45, 49),
-        ["Tab Background"] = Color3.fromRGB(20, 20, 24),
-        ["Tab Border"] = Color3.fromRGB(45, 45, 55),
-        ["Tab Toggle Background"] = Color3.fromRGB(28, 28, 32),
-        ["Section Background"] = Color3.fromRGB(18, 18, 22),
-        ["Section Border"] = Color3.fromRGB(35, 35, 45),
-        ["Text"] = Color3.fromRGB(180, 180, 190),
-        ["Disabled Text"] = Color3.fromRGB(100, 100, 110),
-        ["Object Background"] = Color3.fromRGB(25, 25, 29),
-        ["Object Border"] = Color3.fromRGB(35, 35, 39),
-        ["Dropdown Option Background"] = Color3.fromRGB(19, 19, 23)
     }
 }
 
